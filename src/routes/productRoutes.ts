9523bd730c71742fa294ddee1d14e601a0ab3836
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-13 17:57:14 +0545

    Refactor code structure for improved readability and maintainability

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import productController from "../controllers/productController";
 import isAuthenticate, { UserRole } from "../middlewares/isAuthenticate";
+
+const { multer, storage } = require("../middlewares/multerMiddleware");
+const upload = multer({ storage: storage });
 const router = Router();
 
-router.route("/products").get(productController.getAllProducts).post(isAuthenticate.isAuthenticated, isAuthenticate.restrictTo(UserRole.Admin), productController.createProduct)
+router.route("/products").get(productController.getAllProducts).post(isAuthenticate.isAuthenticated, isAuthenticate.restrictTo(UserRole.Admin), upload.single("productImage"), productController.createProduct);
 router.route("/products/:id").get(productController.getProductById).delete(isAuthenticate.isAuthenticated, isAuthenticate.restrictTo(UserRole.Admin), productController.deleteProductById).patch(isAuthenticate.isAuthenticated, isAuthenticate.restrictTo(UserRole.Admin), productController.updateProductById)
 
 
